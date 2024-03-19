d00946c4149f6e1e19ea27b0b64ec006930cec32
Humberto Dias <hldtux@gmail.com>
2024-03-19 17:12:54 -0300

    add dirent_types

--- newlib/libc/sys/lv2/sys/dirent.h
+++ newlib/libc/sys/lv2/sys/dirent.h
@@ -7,14 +7,14 @@
 
 #define DT_INVALID			0xff
 #define DT_UNKNOWN			0
-#define DT_FIFO				DT_INVALID
-#define DT_CHR				DT_INVALID
-#define DT_DIR				1
-#define DT_BLK				DT_INVALID
-#define DT_REG				2
-#define DT_LNK				3
-#define DT_SOCK				DT_INVALID
-#define DT_WHT				DT_INVALID
+#define DT_FIFO				1
+#define DT_CHR				2
+#define DT_DIR				4
+#define DT_BLK				6
+#define DT_REG				8
+#define DT_LNK				10
+#define DT_SOCK				12
+#define DT_WHT				14
 
 #define dirfd(dp)			((dp)->dd_fd)
 
